eace5faf3c1e27be830ddc531f378887d00d6db3
maryam.bouderham <maryam.bouderham@gmail.com>
2021-09-01 10:54:38 +0000

    variables exemple

---
+++ index.js
@@ -0,0 +1,14 @@
+// const
+const TAILLE=40;
+// TAILLE= 24; Erreur 
+console.log(TAILLE)
+
+// var
+for(var i=0;i<6;i++)
+ console.log(i)
+ console.log(i) // print 6
+
+ //let
+ for(let i=0;i<6;i++)
+ console.log(i)
+ console.log(i) // error
